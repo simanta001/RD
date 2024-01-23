30337124b4099ab732c1dcaa9413356507cbf926
simanta001 <152843642+simanta001@users.noreply.github.com>
2024-01-23 23:15:05 +0530

    calculate sum, product and difference.c

---
+++ calculator sum, product and difference.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    
+    printf("Sum: %d\n", num1 + num2);
+
+   
+    printf("Product: %d\n", num1 * num2);
+
+    
+    printf("Difference: %d\n", num1 - num2);
+
+    return 0;
+}
