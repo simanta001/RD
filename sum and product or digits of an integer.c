026120567763442b36f5780bb8f189b5a5379806
simanta001 <152843642+simanta001@users.noreply.github.com>
2024-01-23 23:26:40 +0530

    sum and product or digits of an integer.c

---
+++ sum and product or digits of an integer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+  
+int main()
+{
+    int n;
+    int dig, sum,pro;
+     
+    printf("\nEnter an integer number :");
+    scanf("%d",&n);
+    sum=0;
+    pro=1;
+     
+    while(n>0)
+    {
+        dig=n%10;
+        sum+=dig;
+        pro*=dig;
+        n=n/10;
+    }
+     
+    printf("\nSUM of all Digits is : %d",sum);
+    printf("\nPRODUCT of all digits: %d",pro);
+     
+    return 0;
+}
